19526339d081ed9b816ed88759074a035c2be5c3
алыа <iseurie@gmail.com>
2017-10-14 22:43:38 -0400

    rely less on shell output for more responsiveness

--- volumearc-widget/volumearc.lua
+++ volumearc-widget/volumearc.lua
@@ -3,11 +3,6 @@ local beautiful = require("beautiful")
 local wibox = require("wibox")
 local timer = require("gears.timer")
 
-local GET_VOLUME_CMD = 'amixer -D pulse sget Master'
-local INC_VOLUME_CMD = 'amixer -D pulse sset Master 5%+'
-local DEC_VOLUME_CMD = 'amixer -D pulse sset Master 5%-'
-local TOG_VOLUME_CMD = 'amixer -D pulse sset Master toggle'
-
 volumearc = wibox.widget {
     max_value = 1,
     thickness = 1,
@@ -24,14 +19,19 @@ volumearc = wibox.widget {
 
 	init = function(self)
 		self.fetch_audio_state(self)
+		self.repaint(self)
 	end,
 
 	update_audio_volume = function(self, delta)
+		local cmd = string.format("amixer -D pulse sset Master %d%%",
+								  math.abs(delta))..((delta > 0) and '+' or '-')
 		self.audio_state.volume = self.audio_state.volume + delta
+		awful.spawn(cmd)
 		self.repaint(self)
 	end,
 
 	toggle_audio_mute = function(self)
+		awful.spawn("amixer -D pulse sset Master toggle")
 		self.audio_state.mute = not self.audio_state.mute
 		self.repaint(self)
 	end,
@@ -46,7 +46,7 @@ volumearc = wibox.widget {
 	end,
 
 	fetch_audio_state = function(self)
-		awful.spawn.easy_async(GET_VOLUME_CMD, function(stdout, stderr, exitreason, exitcode)
+		awful.spawn.easy_async("amixer -D pulse sget Master", function(stdout, stderr, exitreason, exitcode)
 			local volume = string.match(stdout, "(%d?%d?%d)%%")
 			self.audio_state.volume = tonumber(string.format("% 3d", volume))
 			self.audio_state.mute = string.match(stdout, "%[(o%D%D?)%]") == "off"
@@ -62,7 +62,7 @@ volumearc:connect_signal("button::press", function(_, _, _, button)
     elseif (button == 5) then awful.spawn(DEC_VOLUME_CMD, false)
     elseif (button == 1) then awful.spawn(TOG_VOLUME_CMD, false)
     end
-	volumearc_repaint()
+	volumearc.repaint(volumearc)
 	end
 )
 
